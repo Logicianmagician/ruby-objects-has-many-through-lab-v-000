4b639e93ee83fa98794e406230b77b87d347d4c8
Andrew Kairys <andrewkairys@gmail.com>
2018-03-26 15:41:09 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,16 @@
 class Genre
+
+  attr_reader :songs
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+
 end
