f2efebb9e898315ff41150978806d42b7fd56af6
Andrew Kairys <andrewkairys@gmail.com>
2018-03-26 15:30:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,13 @@
 class Artist
-  attr_accessor :songs
-  attr_reader :name
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @songs = []
   end
+
+  def add_song(song)
+    @songs = << song
+    song.artist = self
+
 end
