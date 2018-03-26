f5f5a1217cb6d2883f40ce7f09d592353233a7a1
Andrew Kairys <andrewkairys@gmail.com>
2018-03-26 15:53:39 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,17 @@
 class Patient
 
+
+  attr_reader :appointments
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
   end
 
 end
